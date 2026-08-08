2bf65b4e58d5746028d43a0e793fb9773943867e
Anand Yadav <anand1032yadav@gmail.com>
2026-08-08 18:42:20 +0530

    first

---
+++ Array/Find the Largest Element in an Array.cpp
@@ -0,0 +1,38 @@
+// 1 Approach : Using sort()
+// T.C : O(n log n)
+// S.C : O(1)
+
+class Solution {
+public:
+    int largestElement(vector<int>& nums) {
+
+        sort(nums.begin(),nums.end());
+        int n = nums.size();
+        return nums[n-1];
+
+    }
+};
+
+
+
+// 2 Approach : Linear Scan
+// T.C : O(n)
+// S.C : O(1)
+
+class Solution {
+public:
+    int largestElement(vector<int>& nums) {
+
+        int n = nums.size();
+        int largest = nums[0];
+        for(int i=1; i<n; i++){
+            if(nums[i] > largest){
+                largest = nums[i];
+                break;
+            }
+        }
+        return largest;
+
+    }
+};
+
