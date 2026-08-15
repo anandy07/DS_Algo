3479fdd7cf7eec4c99adc5fc9ac02de5fbbea5b5
Anand Yadav <anand1032yadav@gmail.com>
2026-08-15 17:02:59 +0530

    Sorted + Rotated

--- Array/Check if Array Is Sorted and Rotated.cpp
+++ Array/Check if Array Is Sorted and Rotated.cpp
@@ -31,3 +31,58 @@ public:
         
     }
 };
+
+
+// 2 Approach : Create a Copy of array  and check all the possible circular rotations of original array against sorted array.  
+// T.C : O(n²)
+// S.C : O(n)
+
+
+class Solution {
+public:
+    bool check(vector<int>& nums) {
+
+        int n = nums.size();
+        vector<int> arr = nums;
+        sort(arr.begin(),arr.end());
+
+        for(int x=0; x<=n; x++){
+            bool ans = true;
+            for(int i=0; i<n; i++){
+                if(arr[i] != nums[(i+x)%n]){
+                    ans = false;
+                    break;
+                }
+            }
+            if(ans) return true;
+
+        }
+        return false;
+
+    }
+};
+
+
+
+// 3 Approach : Count the peak element  
+// T.C : O(n)
+// S.C : O(1)
+
+
+class Solution {
+public:
+    bool check(vector<int>& nums) {
+
+        int n = nums.size();
+        int cnt=0;
+
+        for(int i=0; i<n; i++){
+            if(nums[i] > nums[(i+1)%n]){
+                cnt++;
+            }
+        }
+        if(cnt > 1 ) return false;
+        return true;
+
+    }
+};
