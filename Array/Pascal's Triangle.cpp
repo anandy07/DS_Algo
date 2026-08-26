9277bc75dcfe4b9530d7063b709168cad03693eb
Anand Yadav <anand1032yadav@gmail.com>
2026-08-26 15:33:27 +0530

    Not an Easy Problem

---
+++ Array/Pascal's Triangle.cpp
@@ -0,0 +1,32 @@
+// LeetCode Link  :  https://leetcode.com/problems/pascals-triangle/description/
+
+// 1 Approach : 
+// T.C : 
+// S.C :  
+
+
+
+class Solution {
+public:
+    vector<vector<int>> generate(int numRows) {
+        
+        vector<vector<int>> v;
+        v.push_back({1});
+
+        int i=1;
+        while(i < numRows){
+
+            vector<int>row;
+            row.push_back(1);
+
+            for(int j=1; j<i; j++){
+                row.push_back(v[i-1][j-1] + v[i-1][j]);
+            }
+            row.push_back(1);
+            v.push_back(row);
+
+            i++;
+        }
+        return v;
+    }
+};
