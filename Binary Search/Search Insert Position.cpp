3233de59269e423f2b43ad293b37064f8f0af18e
Anand Yadav <anand1032yadav@gmail.com>
2026-09-01 19:45:31 +0530

    Insert

---
+++ Binary Search/Search Insert Position.cpp
@@ -0,0 +1,31 @@
+// LeetCode  : https://leetcode.com/problems/search-insert-position/
+
+// 1 Approach : Binary Search
+// T.C : O(log n)
+// S.C : O(1)
+
+
+
+class Solution {
+public:
+    int searchInsert(vector<int>& nums, int target) {
+
+        int n = nums.size();
+
+        int i=0;
+        int j=n-1;
+
+        while(i <= j){
+            int mid = i+(j-i)/2;
+            if(nums[mid] == target){
+                return mid;
+            }else if(nums[mid] < target){
+                i = mid+1;
+            }else if(nums[mid] > target){
+                j = mid-1;
+            }
+           
+        }
+        return i;
+    }
+};
