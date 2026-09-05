4de70cb92a221f9672af20c206d2414c8ee61479
Anand Yadav <anand1032yadav@gmail.com>
2026-09-05 23:44:57 +0530

    need to do O(log n)

--- Binary Search/Search in Rotated Sorted Array.cpp
+++ Binary Search/Search in Rotated Sorted Array.cpp
@@ -1,8 +1,31 @@
 // LeetCode Link  : https://leetcode.com/problems/search-in-rotated-sorted-array/description/
 
+
 // 1 Approach : 
 // T.C : 
 // S.C : 
+
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+        int n = nums.size();
+
+        int ans=-1;
+        for(int i=0; i<n; i++){
+            if(nums[i] == target){
+                ans = i;
+            }
+        }
+        return ans;
+    
+    }
+};
+
+
+// 2 Approach : 
+// T.C : 
+// S.C : 
+
 
 class Solution {
 public:
