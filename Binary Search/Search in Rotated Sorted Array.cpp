32f38612196be7a108ae9dfb8de9fa5766cb5247
Anand Yadav <anand1032yadav@gmail.com>
2026-09-05 23:33:22 +0530

    O(log n)

---
+++ Binary Search/Search in Rotated Sorted Array.cpp
@@ -0,0 +1,22 @@
+// LeetCode Link  : https://leetcode.com/problems/search-in-rotated-sorted-array/description/
+
+// 1 Approach : 
+// T.C : 
+// S.C : 
+
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+
+        int n = nums.size();
+
+        map<int, int> mp;
+        for (int i = 0; i < n; i++) {
+            mp[nums[i]] = i;
+        }
+        if (mp.find(target) == mp.end()) {
+            return -1;
+        }
+        return mp[target];
+    }
+};
