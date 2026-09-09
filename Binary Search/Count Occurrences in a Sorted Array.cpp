500030f8dce953c4670f052a68ae7ec78aefca45
Anand Yadav <anand1032yadav@gmail.com>
2026-09-09 23:04:10 +0530

    Solve

---
+++ Binary Search/Count Occurrences in a Sorted Array.cpp
@@ -0,0 +1,8 @@
+/*
+
+You are given a sorted array of integers arr and an integer target. Your task is to determine how many times target appears in arr.
+Return the count of occurrences of target in the array.
+
+
+*/
+
