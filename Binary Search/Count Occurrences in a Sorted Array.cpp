d73b2bfb81dbcd1480d1061e913563b776048610
Anand Yadav <anand1032yadav@gmail.com>
2026-09-11 15:10:13 +0530

    Count

--- Binary Search/Count Occurrences in a Sorted Array.cpp
+++ Binary Search/Count Occurrences in a Sorted Array.cpp
@@ -6,3 +6,49 @@ Return the count of occurrences of target in the array.
 
 */
 
+
+// 1 Approach : Binary Search
+// T.C : O(log n)
+// S.C : O(1)
+
+
+
+class Solution {
+public:
+    int countOccurrences(vector<int>& nums, int target) {
+        // Your code goes here
+        int n = nums.size();
+
+        int i=0;
+        int j=n-1;
+        int first=0;
+        int second=0;
+
+        while(i <= j){
+            int mid = i+(j-i)/2;
+            if(nums[mid] == target){
+                second=mid;
+                i = mid+1;
+            }else if(nums[mid] < target){
+                i = mid+1;
+            }else{
+                j = mid-1;
+            }
+        }
+        i =0;
+        j = n-1;
+
+        while(i<=j){
+            int mid = i+(j-i)/2;
+            if(nums[mid] == target){
+                first=mid;
+                j = mid-1;
+            }else if(nums[mid] < target){
+                i = mid+1;
+            }else{
+                j = mid-1;
+            }
+        }
+        return second-first+1;;
+    }
+};
