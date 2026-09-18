ec4f43c0ec49a1e2f5674c7efade9196246f402c
Anand Yadav <anand1032yadav@gmail.com>
2026-09-18 21:43:26 +0530

    Need to done

--- Binary Search/Find Peak Element.cpp
+++ Binary Search/Find Peak Element.cpp
@@ -1,1 +1,28 @@
-//       https://leetcode.com/problems/find-peak-element/description/
+// LeetCode Link :   https://leetcode.com/problems/find-peak-element/description/
+
+
+// 1 Approach : Brute Force
+// T.C : O(n)
+// S.C : O(1)
+
+
+
+class Solution {
+public:
+    int findPeakElement(vector<int>& nums) {
+
+        int n = nums.size();
+
+        int idx=0;
+        int ans=INT_MIN;
+
+        for(int i=0; i<n; i++){
+            if(nums[i] > ans){
+                ans = nums[i];
+                idx = i;
+            }
+        }
+        return idx;
+
+    }
+};
