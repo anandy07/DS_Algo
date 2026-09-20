563a28ec74de382e23fd4983c3d6c695d89f1b6d
Anand Yadav <anand1032yadav@gmail.com>
2026-09-20 23:49:42 +0530

    koko

--- Binary Search/Koko Eating Bananas.cpp
+++ Binary Search/Koko Eating Bananas.cpp
@@ -1,7 +1,35 @@
 // LeetCode  Link : https://leetcode.com/problems/koko-eating-bananas/description/
 
-// 1 Approach : 
+
+// 1 Approach : Brute Force (TLE)
 // T.C : 
 // S.C : 
 
 
+class Solution {
+public:
+    int minEatingSpeed(vector<int>& piles, int h) {
+
+        int n = piles.size();
+        int k=1;
+
+        while(true){
+            long long t= 0;
+            for(int i=0; i<n; i++){
+                int num = piles[i];
+                long long cnt=0;
+                while(num > 0){
+                    cnt += 1;
+                    num = num - k;
+                }
+                t += cnt;
+            }
+            if( t <= h){
+                return k;
+            }
+            k++;
+        }
+        return -1;
+        
+    }
+};
