ddc32024eab069943fd6f12c1e7f448e2e7a50f3
Anand Yadav <anand1032yadav@gmail.com>
2026-09-21 22:47:19 +0530

    xx

--- Binary Search/Koko Eating Bananas.cpp
+++ Binary Search/Koko Eating Bananas.cpp
@@ -38,3 +38,34 @@ public:
 // 2 Approach : 
 // T.C : 
 // S.C : 
+
+
+class Solution {
+public:
+    int minEatingSpeed(vector<int>& piles, int h) {
+
+        int n = piles.size();
+        int i = 1;
+        long long j = *max_element(piles.begin(), piles.end());
+        int ans=-1;
+
+        while( i <= j){
+            long long t=0;
+            int mid = i+(j-i)/2;
+            for(int i=0; i<n; i++){
+                t += piles[i]/mid;
+                if(piles[i]%mid != 0){
+                    t++;
+                }
+            }
+            if(t <= h){
+                ans =  mid;
+                j = mid-1;
+            }else{
+                i = mid+1;
+            }
+            
+        }
+        return ans;
+    }
+};
